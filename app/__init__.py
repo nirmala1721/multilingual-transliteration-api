7d01cd0416aab0624e080f1f4326b5a09e887edd
Nirmala Bigimalla <nirmalabigimalla@gmail.com>
2026-09-26 14:48:17 +0530

    Enable CORS for deployed frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from app.api import api
 from app.config import MAX_FILE_SIZE
@@ -10,6 +11,18 @@ def create_app():
     app = Flask(__name__)
 
     app.config["MAX_CONTENT_LENGTH"] = MAX_FILE_SIZE
+
+    CORS(
+        app,
+        resources={
+            r"/api/*": {
+                "origins": [
+                    "https://multilingual-transliteration-api.vercel.app",
+                    "http://localhost:5173",
+                ]
+            }
+        }
+    )
 
     limiter.init_app(app)
 
